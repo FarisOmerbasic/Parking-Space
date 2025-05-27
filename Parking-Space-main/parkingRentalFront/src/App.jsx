375d452f5487064688233653301382f6f76e4532
Ahmed Ajanovic <ajanovicahmed11@hotmail.com>
2025-05-27 21:43:23 +0200

    add username beside profile icon

--- Parking-Space-main/parkingRentalFront/src/App.jsx
+++ Parking-Space-main/parkingRentalFront/src/App.jsx
@@ -1,16 +1,10 @@
-import {
-  Routes,
-  Route,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
 import UserProfile from "./pages/UserProfile";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
 import ListSpace from "./pages/ListSpace";
-import Settings from "./pages/Settings"; // Fixed typo here
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import AppLayout from "./pages/AppLayout";
@@ -20,37 +14,29 @@ import AllSpaces from "./pages/AllSpaces";
 import MapAllSpaces from "./components/MapAllSpaces";
 
 export default function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        element: <AppLayout />,
-        children: [
-          { path: "/", element: <Homepage /> },
-          { path: "/booking", element: <Booking /> },
-          { path: "/myBookings", element: <MyBookings /> },
-          { path: "/settings", element: <Settings /> },
-          { path: "/all-spaces", element: <AllSpaces /> },
-          { path: "/map", element: <MapAllSpaces /> },
-          {
-            element: <ProfileLayout />,
-            children: [
-              { path: "/profile", element: <UserProfile /> },
-              { path: "/listSpace", element: <ListSpace /> },
-              { path: "/dashboard", element: <Dashboard /> },
-              { path: "/mySpaces", element: <MySpaces /> },
-            ],
-          },
-        ],
-      },
-      { path: "/login", element: <Login /> },
-      { path: "/register", element: <Register /> },
-    ],
+  const router = createBrowserRouter([
     {
-      future: {
-        v7_startTransition: true,
-      },
-    }
-  );
+      element: <AppLayout />,
+      children: [
+        { index: true, element: <Homepage /> },
+        { path: "/booking", element: <Booking /> },
+        { path: "/my-bookings", element: <MyBookings /> },
+        { path: "/all-spaces", element: <AllSpaces /> },
+        { path: "/map", element: <MapAllSpaces /> },
+        {
+          element: <ProfileLayout />,
+          children: [
+            { path: "/profile", element: <UserProfile /> },
+            { path: "/dashboard", element: <Dashboard /> },
+            { path: "/list-space", element: <ListSpace /> },
+            { path: "/my-spaces", element: <MySpaces /> },
+          ],
+        },
+      ],
+    },
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+  ]);
 
   return <RouterProvider router={router} />;
 }
